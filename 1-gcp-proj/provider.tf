e6722a76d2e9e69c1c5e9641c39ff6c725a049aa
devopsvj <devopsvj@gmail.com>
2021-02-04 21:54:09 -0500

    Delete 1-gcp-proj directory

--- 1-gcp-proj/provider.tf
+++
@@ -1,7 +0,0 @@
-provider "google" {
-  version = "~> 3.5.0"
-  credentials = file("/<DIR_PATH>/<Service_Account_File_Name>.json")
-  project = "<Project_ID>"
-  region  = "us-central1"
-  zone    = "us-central1-c"
-}
