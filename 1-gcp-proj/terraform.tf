e8129d810b3c40b09ba29a9f638164997baa5222
devopsvj <devopsvj@gmail.com>
2021-02-04 21:44:11 -0500

    Update terraform.tf

--- 1-gcp-proj/terraform.tf
+++ 1-gcp-proj/terraform.tf
@@ -12,6 +12,6 @@ terraform {
     backend "gcs" {
         bucket = "gcp-dev-tfstate"
         prefix =  "gcp/tfstate/dev/"
-        credentials = "~/Documents/Google-GCP-2021/GCP-ServiceAccouts/devopsvj-project/devopsvj-tf-sa/poised-climate-303823-8d82e66fede9.json"
+        credentials = "~/<DIR_PATH>/<Service_Account_File_Name>.json"
     }  
 }
